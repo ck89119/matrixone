bd03463d58ae82e6ed09379b392a1e05d1789dc3
Cao Kai <ck89119@gmail.com>
2024-06-06 17:21:44 +0800

    add bvt

--- test/distributed/cases/snapshot/sys_restore_to_nonsys_account.sql
+++ test/distributed/cases/snapshot/sys_restore_to_nonsys_account.sql
@@ -216,8 +216,8 @@ drop database acc_test03;
 -- @session
 drop snapshot sp04;
 
-select * from mo_catalog.mo_tables where relname = 'acc_test02' and reldatabase = 'pri01';
-select * from mo_catalog.mo_tables where relname = 'acc_test02' and reldatabase = 'aff01';
+select * from mo_catalog.mo_tables where relname = 'pri01' and reldatabase = 'acc_test02';
+select * from mo_catalog.mo_tables where relname = 'aff01' and reldatabase = 'acc_test02';
 -- @session:id=1&user=acc01:test_account&password=111
 show databases;
 select count(*) from acc_test02.pri01;
@@ -241,8 +241,8 @@ insert into table02 (col1, col2) values (133, 'database');
 drop snapshot if exists sp07;
 create snapshot sp07 for account acc01;
 
-select * from mo_catalog.mo_tables where relname = 'acc_test02' and reldatabase = 'pri01';
-select * from mo_catalog.mo_tables where relname = 'acc_test02' and reldatabase = 'aff01';
+select * from mo_catalog.mo_tables where relname = 'pri01' and reldatabase = 'acc_test02';
+select * from mo_catalog.mo_tables where relname = 'aff01' and reldatabase = 'acc_test02';
 -- @session:id=1&user=acc01:test_account&password=111
 show databases;
 select count(*) from acc_test02.pri01;
